c52fd80ad3ea3b2e91700d2dfad50c1e211e69a4
Swap <mindstixsoftwarelabs@wp.local>
2016-12-19 01:41:07 +0530

    Added UIPicker,BuyNow button

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource  {
 
+    @IBOutlet weak var stateBtn: UIButton!
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    let states = ["New York", "California", "Boston", "New Orleans", "Washington DC"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(sender: AnyObject) {
+        statePicker.hidden = false
+    }
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+     return 1
+    }
 
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        stateBtn.setTitle(states[row], forState: UIControlState.Normal)
+        statePicker.hidden = true
+    }
+    
+    
+    
+    
+    
 }
 
